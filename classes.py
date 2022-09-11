46582cbe22c041df37792f4bc5b54765227578e3
Juan Ballén <91291820+JCBallen@users.noreply.github.com>
2022-09-11 14:15:38 -0500

    sum of cards and blackjack recognition

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+from ast import Return
 import os
 import random
 
@@ -36,8 +37,9 @@ class Deck:
 
         for suit in suits:
             for rank in ranks:
-                self.cards.append(Card(suit, rank))  # basicamente estamos creando objetos carta y luego appendeando esos objetos al Deck.cards (queda una lista de objetos)
-                                                     # si vieramos el deck se veria ahora algo como esto: ['J of spades', '3 of hearts', Q of clubs, 10 of diamonds, ...]
+                # basicamente estamos creando objetos carta y luego appendeando esos objetos al Deck.cards (queda una lista de objetos)
+                self.cards.append(Card(suit, rank))
+                # si vieramos el deck se veria ahora algo como esto: ['J of spades', '3 of hearts', Q of clubs, 10 of diamonds, ...]
 
     def shuffleCards(self):   # revuelve las cartas :V
         if len(self.cards) <= 1:
@@ -54,13 +56,15 @@ class Deck:
             cards_dealt.append(self.cards.pop())
         return cards_dealt
 
-# -------------------------------------------------------------- Test 
+
+# -------------------------------------------------------------- Test
 deck1 = Deck()
 deck2 = Deck()
 deck2.shuffleCards()
 
-# NOTA: se ven como listas de objetos, si se desea "ver claramente" abajo hay un ejemplo de como visualizarlo 
-print('Deck 1:\n', deck1.cards)  # deck1 no esta revuelta (no shuffled)
+# NOTA: se ven como listas de objetos, si se desea "ver claramente" abajo hay un ejemplo de como visualizarlo
+# deck1 no esta revuelta (no shuffled) (ya que es una lista de objetos, solo podemos ver 1 objeto bien a la vez, abajo hay un truco para ver todos)
+print('Deck 1:\n', deck1.cards[0])
 # print(deck1.cards[1][1]['value'])  # carta indice 1, el dict de ranks, el valor del rank
 print('\nDeck 2:\n', deck2.cards)  # deck2 esta revuelta (suffled)
 
@@ -75,6 +79,8 @@ respond = deck2.shuffleCards()
 print('\nShuffle Deck 2:\n', respond, deck2.cards)
 
 # ---------------------------------------------------- Clase hand
+
+
 class Hand:
     def __init__(self, dealer=False):
         self.cards = []
@@ -84,15 +90,45 @@ class Hand:
     def add_card(self, card_list):
         self.cards.extend(card_list)
 
+    def calculate_value(self):     # suma las cartas
+        self.value = 0
+        hasAce = False
+
+        for card in self.cards:
+            if card.rank['rank'] == 'A':
+                hasAce = True                      # avisa si leyo una A
+            self.value += card.rank['value']       # se suma normal, luego lo arreglamos
+
+        if hasAce and self.value > 21:       # si existia una A y se pasa de 21, hacemos que solo valga 1, restandole 10
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()                # retorna el valor de las cartas primero llamando a calculate_value, y ahi si
+        return self.value
+
+    def is_blackjack(self):
+        self.get_value()
+        if self.value == 21:
+            return "IT'S A BLACKJACK"
+        return "It's not a BlackJack"
+
+
 # --------------------------------------------------Test
 deck5 = Deck()
 deck5.shuffleCards()     # Creamos un nuevo mazo y lo revolvemos
 
 hand1 = Hand()
-hand1.add_card(deck5.dealCard(2))   # Creamos una mano y le damos 2 cartas (por ser blackjack)
+# Creamos una mano y le damos 2 cartas (por ser blackjack)
+hand1.add_card(deck5.dealCard(2))
 
-mapaDelDeck = map(lambda n:str(n),deck5.cards)          # Trucoteca para visualizar los objetos dentro de una lista, aprovecahndo el __str__ que le pusimos a Card
-print('\nDeck 5:\n',list(mapaDelDeck))                  # ya que si lo vieramos directamente se ve asi: [<main.object>, <main.object>, <main.object>]    
+# Trucoteca para visualizar los objetos dentro de una lista, aprovecahndo el __str__ que le pusimos a Card
+mapaDelDeck = map(lambda n: str(n), deck5.cards)
+# ya que si lo vieramos directamente se ve asi: [<main.object>, <main.object>, <main.object>]
+print('\nDeck 5:\n', list(mapaDelDeck))
 
-mapaDeHand = map(lambda n:str(n),hand1.cards)
-print('\nMano 1:\n',list(mapaDeHand))
+mapaDeHand = map(lambda n: str(n), hand1.cards)
+print('\nMano 1:\n', list(mapaDeHand))
+
+sumOfHand = hand1.get_value()
+print(f'The sum of the cards is {sumOfHand}')
+print(hand1.is_blackjack())
